6723857161528e2e6220b8b9762468c1891d2629
tungyao <1922527051@qq.com>
2020-12-05 11:43:35 +0800

    first commit

---
+++ ONI_TONG/Base.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using Database;
+using Harmony;
+using STRINGS;
+using UnityEngine;
+
+namespace ONI_TONG
+{
+    // 将电线的最大电压增大10倍     // 修改电线输出功率
+    [HarmonyPatch(typeof(BaseWireConfig))]
+    [HarmonyPatch("DoPostConfigureComplete", new Type[] {typeof(Wire.WattageRating), typeof(GameObject)})]
+    internal class EnumWire
+    {
+        static void Postfix(BaseWireConfig __instance, Wire.WattageRating rating, GameObject go)
+        {
+            Wire component = go.GetComponent<Wire>();
+            switch (rating)
+            {
+                case Wire.WattageRating.Max500:
+                    break;
+                case Wire.WattageRating.Max1000:
+                    break;
+                case Wire.WattageRating.Max2000:
+                    break;
+                case Wire.WattageRating.Max20000:
+                    break;
+                case Wire.WattageRating.Max50000:
+                    break;
+                default:
+                    rating = Wire.WattageRating.Max50000;
+                    break;
+            }
+
+            component.MaxWattageRating = rating;
+            float maxWattageAsFloat = Wire.GetMaxWattageAsFloat(rating);
+            Descriptor item = default(Descriptor);
+            item.SetupDescriptor(
+                string.Format(UI.BUILDINGEFFECTS.MAX_WATTAGE,
+                    GameUtil.GetFormattedWattage(maxWattageAsFloat, GameUtil.WattageFormatterUnit.Automatic)),
+                string.Format(UI.BUILDINGEFFECTS.TOOLTIPS.MAX_WATTAGE, new object[0]),
+                Descriptor.DescriptorType.Effect);
+            Building component2 = go.GetComponent<Building>();
+            BuildingDef def = component2.Def;
+            if (def.EffectDescription == null)
+            {
+                def.EffectDescription = new List<Descriptor>();
+            }
+
+            def.EffectDescription.Add(item);
+        }
+    }
+
+    [HarmonyPatch(typeof(Wire), "GetMaxWattageAsFloat", new Type[] {typeof(Wire.WattageRating)})]
+    internal class SuperToilet
+    {
+        static void Postfix(ref float __result, Wire.WattageRating rating)
+        {
+            switch (rating)
+            {
+                case Wire.WattageRating.Max500:
+                    __result = 500f;
+                    break;
+                case Wire.WattageRating.Max1000:
+                    __result = 1000f;
+                    break;
+                case Wire.WattageRating.Max2000:
+                    __result = 20000f;
+                    break;
+                case Wire.WattageRating.Max20000:
+                    __result = 20000f;
+                    break;
+                case Wire.WattageRating.Max50000:
+                    __result = 100000f;
+                    break;
+                default:
+                    __result = 100000f;
+                    break;
+            }
+        }
+    }
+
+    // 新增超级高压线
+    [HarmonyPatch(typeof(GeneratedBuildings), "LoadGeneratedBuildings")]
+    internal class SuperWire
+    {
+        static void Prefix()
+        {
+            AddBuildingStrings("SuperHighWattageWire", "Super High Wattage Wire",
+                "that's cool",
+                "Golden Legend ");
+        }
+
+        static void AddBuildingStrings(string buildingId, string name, string description, string effect)
+        {
+            Strings.Add(new string[]
+            {
+                "STRINGS.BUILDINGS.PREFABS." + buildingId.ToUpperInvariant() + ".NAME",
+                UI.FormatAsLink(name, buildingId)
+            });
+            Strings.Add(new string[]
+            {
+                "STRINGS.BUILDINGS.PREFABS." + buildingId.ToUpperInvariant() + ".DESC",
+                description
+            });
+            ModUtil.AddBuildingToPlanScreen("Power", "SuperHighWattageWire");
+        }
+
+        static void AddBuildingToPlanScreen(HashedString category, string buildingId, string addAfterBuildingId = null)
+        {
+            int num = TUNING.BUILDINGS.PLANORDER.FindIndex((PlanScreen.PlanInfo x) => x.category == category);
+            if (num == -1)
+            {
+                return;
+            }
+
+            IList<string> list = TUNING.BUILDINGS.PLANORDER[num].data as IList<string>;
+            if (list == null)
+            {
+                Debug.Log("Could not add " + buildingId + " to the building menu.");
+                return;
+            }
+
+            int num2 = list.IndexOf(addAfterBuildingId);
+            if (num2 != -1)
+            {
+                list.Insert(num2 + 1, buildingId);
+                return;
+            }
+
+            list.Add(buildingId);
+        }
+    }
+
+    [HarmonyPatch(typeof(Db))]
+    [HarmonyPatch("Initialize")]
+    internal class DbInitializePatch
+    {
+        // Token: 0x06000047 RID: 71 RVA: 0x00003480 File Offset: 0x00001680
+        public static void Prefix(Techs __this)
+        {
+            Tech tech28 = new Tech("AdvancedPowerRegulation", new string[7]
+            {
+                "HydrogenGenerator",
+                "HighWattageWire",
+                "WireBridgeHighWattage",
+                "PowerTransformerSmall",
+                "SuperHighWattageWire",
+                LogicPowerRelayConfig.ID,
+                LogicWattageSensorConfig.ID
+            }, __this);
+        }
+    }
+}
---
+++ ONI_TONG/SuperHighWattage.cs
@@ -0,0 +1,32 @@
+﻿using TUNING;
+using UnityEngine;
+
+namespace ONI_TONG
+{
+    public class SuperHighWattageWireConfig :BaseWireConfig
+    {
+        public override BuildingDef CreateBuildingDef()
+        {
+            string id = this.GetId();
+            string anim = "utilities_tong_pics_kanim";
+            float construction_time = 2f;
+            float[] tier = BUILDINGS.CONSTRUCTION_MASS_KG.TIER2;
+            float insulation = 0f;
+            EffectorValues none = NOISE_POLLUTION.NONE;
+            BuildingDef buildingDef = base.CreateBuildingDef(id, anim, construction_time, tier, insulation, BUILDINGS.DECOR.PENALTY.TIER5, none);
+            return buildingDef;
+        }
+
+        public override void DoPostConfigureComplete(GameObject go)
+        {
+            base.DoPostConfigureComplete(Wire.WattageRating.NumRatings, go);
+        }
+        public const string Id = "SuperHighWattageWire";
+        public const string DisplayName = "Super High Wattage Wire";
+        public const string Description = "Golden Legend";
+        protected virtual string GetId()
+        {
+            return "SuperHighWattageWire";
+        }
+    }
+}
